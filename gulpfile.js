b856fa6869db0f39f589f2293fb40e27b3f6c0e5
Marek Błaszczyk <marek.blaszczyk@gmail.com>
2016-02-25 20:03:58 +0100

    Finish with minify and uglify from 3rd party packages

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,9 @@
 
 var gulp = require('gulp');
 var mainBowerFiles = require('main-bower-files');
+var minifycss = require('gulp-minify-css');
 var plugins = require('gulp-load-plugins')({
-  pattern: ['gulp-*', 'gulp.*', 'main-bower-files'],
+  pattern: ['gulp-*', 'gulp.*', 'main-bower-files', 'gulp-minify-css'],
   replaceString: /\bgulp[\-.]/
   // gulp-open, gulp-connect ....
 });
@@ -11,6 +12,7 @@ var browserify = require('browserify'); // Bundles JS
 var reactify = require('reactify'); // Transforms React JSX to JS
 var source = require('vinyl-source-stream'); // Use conventional text streams with Gulp
 var transform = require('vinyl-transform');
+
 
 
 // Config dev server
@@ -35,33 +37,24 @@ gulp.task('html', function() {
 
 // vendors
 gulp.task('vendors', function() {
-//    plugins.util.log(mainBowerFiles('**/*.js'));
+//    plugins.util.log(mainBowerFiles('**/*'));
 
-    var jsFilter = plugins.filter('**/*.js', { restore: true });
-    var cssFilter = plugins.filter('*.css', { restore: true });
+    var jsFilter = plugins.filter('**/**/*.js', { restore: true });
+    var cssFilter = plugins.filter('**/**/*.css', { restore: true });
 
     gulp.src(plugins.mainBowerFiles())
         .pipe(jsFilter)
-        .pipe(plugins.uglify())
-        .pipe(plugins.rename({ suffix: ".min"}))
+        .pipe(plugins.if('!*.min.js', plugins.uglify()))
+        .pipe(plugins.if('!*.min.js', plugins.rename({ suffix: ".min"})))
+        .pipe(plugins.concat('combined.min.js'))
         .pipe(gulp.dest(config.paths.dist + '/vendors/js'))
         .pipe(jsFilter.restore)
         .pipe(cssFilter)
-        .pipe(plugins.minifycss())
+        .pipe(plugins.if('!*.min.css', minifycss()))
+        .pipe(plugins.if('!*.min.css', plugins.rename({ suffix: ".min"})))
+        .pipe(plugins.concat('combined.min.css'))
         .pipe(gulp.dest(config.paths.dist + '/vendors/css'))
-
-
-//    browserify(plugins.mainBowerFiles('**/*.js'))
-//        .bundle()
-//        .pipe(source('vendors.js'))
-//        .pipe(gulp.dest(config.paths.dist + '/vendors/js'))
-//        .pipe(plugins.connect.reload());
-
-//    browserify(plugins.mainBowerFiles('**/*.css'))
-//        .bundle()
-//        .pipe(source('vendors.css'))
-//        .pipe(gulp.dest(config.paths.dist + '/vendors/css'))
-//        .pipe(plugins.connect.reload());
+        .pipe(plugins.connect.reload());
 });
 
 
